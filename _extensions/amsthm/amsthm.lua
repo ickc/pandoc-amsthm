975e3feeee967bbcff2bb8ba57fc33ac1a7fb127
Kolen Cheung <10970208+ickc@users.noreply.github.com>
2026-09-24 03:32:08 +0100

    Revert "Leave references to unnumbered environments unresolved in LaTeX"
    This reverts commit 5668e8cd5fc2ea6f8176e70688fade2d953a5548.

--- _extensions/amsthm/amsthm.lua
+++ _extensions/amsthm/amsthm.lua
@@ -755,12 +755,10 @@ end
 M.resolve_inline = resolve_inline
 
 -- LaTeX: collect ids (pass 1) and emit \begin{env}…\end{env} (pass 2).
--- Only numbered environments, as in other output: \ref to an unnumbered
--- one would print the number of the enclosing section.
 local function collect_ref_id(div, options)
   local theorem = find_theorem(options, div.classes, true)
   if not theorem then return nil end
-  if theorem.numbered and div.identifier and div.identifier ~= "" then
+  if div.identifier and div.identifier ~= "" then
     options.identifiers[div.identifier] = ""
     options.names[div.identifier] = theorem.text
   end
